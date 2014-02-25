a7312cd9893a7620c95f452f813a42ca4c172897
alexwasner <alex.wasner@gmail.com>
2014-02-24 21:01:40 -0600

    Got auth working for twitter

--- src/android/ParseTwitter.java
+++ src/android/ParseTwitter.java
@@ -18,30 +18,31 @@ import com.parse.ParseException;
 import com.parse.ParseTwitterUtils;
 import com.parse.ParseUser;
 
+import android.content.Context;
 import android.util.Log;
 
 public class ParseTwitter extends CordovaPlugin {
 
   public static final String ACTION_INIT = "init";
-	
+  
   public static final String ACTION_LOGIN = "login";
-	
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if(ACTION_INIT.equals(action)){
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if(ACTION_INIT.equals(action)){
       this.initialize(callbackContext, args);
       return true;
-		}
-		else if (ACTION_LOGIN.equals(action)){
+    }
+    else if (ACTION_LOGIN.equals(action)){
       this.login(callbackContext, args);
       return true;
-		}
-		return false;
-	}
+    }
+    return false;
+  }
 
   private void initialize(final CallbackContext callbackContext, final JSONArray args) {
       cordova.getThreadPool().execute(new Runnable() {
-    	  public void run() {
+        public void run() {
               try {
                   String appId = args.getString(0);
                   String clientKey = args.getString(1);
@@ -55,23 +56,27 @@ public class ParseTwitter extends CordovaPlugin {
   }
 
   private void login(final CallbackContext callbackContext, final JSONArray args) {
-      cordova.getThreadPool().execute(new Runnable() {
+//    Context context=this.cordova.getActivity()
+    cordova.getActivity().runOnUiThread(new Runnable() {
           public void run() {
-				ParseTwitterUtils.logIn(cordova.getActivity().getApplicationContext(), new LogInCallback() {
-				@Override
-				public void done(ParseUser user, ParseException err) {
-				    if (user == null) {
-				      Log.d("VapeSlate", "The user cancelled the Twitter login.");
-				    } else if (user.isNew()) {
-				      Log.d("VapeSlate", "User signed up and logged in through Twitter.");
-				    } else {
-				      Log.d("VapeSlate", "User logged in through Twitter.");
-				    }
-				    callbackContext.success();
-				}
-			});
+        String consumerKey = "Y0urTw1tt3rC0nsum3rK3y";
+        String consumerSecret ="Y0urTw1tt3rC0nsum3r53cr3t";
+        ParseTwitterUtils.initialize(consumerKey,consumerSecret);
+        ParseTwitterUtils.logIn(cordova.getActivity(), new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException err) {
+            if (user == null) {
+              Log.d("VapeSlate", "The user cancelled the Twitter login.");
+            } else if (user.isNew()) {
+              Log.d("VapeSlate", "User signed up and logged in through Twitter.");
+            } else {
+              Log.d("VapeSlate", "User logged in through Twitter.");
+            }
+            callbackContext.success();
+        }
+      });
           }
       });
   }
-	
+  
 }
--- www/parsetwitter.js
+++ www/parsetwitter.js
@@ -5,12 +5,13 @@ var ParseTwitter = function () {
     this.name = "ParseTwitter";
 };
 
+ParseTwitter.prototype.init = function(appId, clientKey, success, error) {
+  cordova.exec(success, error, 'ParseTwitter', 'init', [appId, clientKey]);
+};
+
 ParseTwitter.prototype.login = function(success, error) {
   cordova.exec(success, error, 'ParseTwitter', 'login', []);
 };
 
-ParseTwitter.prototype.init = function(appId, clientKey, success, error) {
-  cordova.exec(success, error, 'ParseTwitter', 'init', [appId, clientKey]);
-};
 
 module.exports = new ParseTwitter();
